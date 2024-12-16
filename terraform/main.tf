0c75851a9635cd642b5bce99bcc24958c3f38b9e
Daud Semab <daudsemab.dxb@gmail.com>
2024-12-16 18:05:08 +0500

    deleted terraform.tf and added remote storage in main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -5,6 +5,13 @@ terraform {
       version = "=4.1.0"
     }
   }
+
+  backend "azurerm" { # Step-2 to setup remote state.
+    resource_group_name  = var.resource_group_name
+    storage_account_name = var.storage_account_name
+    container_name       = var.storage_container_name
+    key                  = "terraform.tfstate"
+  }
 }
 
 provider "azurerm" {
