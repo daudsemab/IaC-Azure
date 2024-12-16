ead27872c04d7c9cfa7a146c77bb63ed7e2c6287
Daud Semab <daudsemab.dxb@gmail.com>
2024-12-16 13:36:01 +0500

    added required provider

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,6 +1,18 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 3.0.2"
+    }
+  }
+
+  required_version = ">= 1.1.0"
+}
+
 provider "azurerm" {
   features {}
 }
+
 
 resource "azurerm_storage_account" "tfstate" { # Step-1 to setup remote state.
   name                            = var.storage_account_name
